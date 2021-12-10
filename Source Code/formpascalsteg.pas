2fd38d5f026a5194c6128daeb898b6af176f0432
Andrew Virya Victorio <andrew.avv03@gmail.com>
2021-12-10 08:51:04 +0700

    Revert "Merge 'development' branch to main (#5)"
    This reverts commit dadf1cf8ecafaf05fdea36b1af587e3bc02b2136.

--- Source Code/formpascalsteg.pas
+++ Source Code/formpascalsteg.pas
@@ -5,38 +5,6 @@ unit FormPascalSteg;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtDlgs, Buttons,
-  StdCtrls, ExtCtrls;
-
-type
-
-  { TForm1 }
-
-  TForm1 = class(TForm)
-    EXTRACTION: TLabel;
-    embedImage2: TImage;
-    EMBEDDING: TLabel;
-    labelOriginalImage1: TLabel;
-    labelOriginalImage2: TLabel;
-    labelOriginalImage3: TLabel;
-    loadButton: TButton;
-    radioLoadOption: TRadioGroup;
-    saveButton: TButton;
-    closeButton: TButton;
-    embedText: TEdit;
-    extractText: TEdit;
-    embedButton: TButton;
-    extractButton: TButton;
-    originalImage1: TImage;
-    embedImage1: TImage;
-    openPictureDialog: TOpenPictureDialog;
-    savePictureDialog: TSavePictureDialog;
-    procedure embedTextChange(Sender: TObject);
-    procedure embedButtonClick(Sender: TObject);
-    procedure FormCreate(Sender: TObject);
-    procedure originalImage1Click(Sender: TObject);
-    procedure embedImage1Click(Sender: TObject);
-    
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
 
 type
@@ -54,32 +22,5 @@ implementation
 
 {$R *.lfm}
 
-{ TForm1 }
-
-procedure TForm1.FormCreate(Sender: TObject);
-begin
-
-end;
-
-procedure TForm1.originalImage1Click(Sender: TObject);
-begin
-
-end;
-
-procedure TForm1.embedImage1Click(Sender: TObject);
-begin
-
-end;
-
-
-procedure TForm1.embedButtonClick(Sender: TObject);
-begin
-
-end;
-
-procedure TForm1.embedTextChange(Sender: TObject);
-begin
-
-end;
 end.
 
