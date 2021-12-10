f4b2875884adcde166b9d70fd1fc1b221fa461a7
Andrew Virya Victorio <andrew.avv03@gmail.com>
2021-12-10 09:38:00 +0700

    reset: soft reset 'development' to 09db8dd...

--- Source Code/formpascalsteg.pas
+++ Source Code/formpascalsteg.pas
@@ -36,6 +36,11 @@ type
     procedure FormCreate(Sender: TObject);
     procedure originalImage1Click(Sender: TObject);
     procedure embedImage1Click(Sender: TObject);
+    
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+
+type
+  TForm1 = class(TForm)
   private
 
   public
@@ -76,6 +81,5 @@ procedure TForm1.embedTextChange(Sender: TObject);
 begin
 
 end;
-
 end.
 
